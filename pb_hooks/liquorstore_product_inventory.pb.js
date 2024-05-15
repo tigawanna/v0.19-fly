2bb1fce8f1fcb55b819108307e80d9534edb3623
tigawanna <denniskinuthiawaweru@gmail.com>
2024-05-15 16:20:42 +0300

    added inventory validations

--- pb_hooks/liquorstore_product_inventory.pb.js
+++ pb_hooks/liquorstore_product_inventory.pb.js
@@ -26,6 +26,10 @@
 
 //  pocketbase hook to update the liquorstore_product count whenever uitems in prdoduct_inventory are added/ rmoved
 
+//  ==============================================================
+// =======================  create  hook ==========================
+//  ==============================================================
+
 onRecordBeforeCreateRequest((e) => {
   // const variant = JSON.parse(e.record?.get("variant"));
   const productID = e.record?.get("product");
@@ -44,18 +48,18 @@ onRecordBeforeCreateRequest((e) => {
   //   console.log(" ==== quantity ==== ",productQty)
   //   console.log(" ==== size==== ",productSize)
 
-  //   fupdate the record accordingly
+  //   update the record accordingly
   const record = $app.dao().findRecordById("liquorstore_products", productID);
-  console.log(" ==== record ==== ", record);
+  // console.log(" ==== record ==== ", record);
   if (!record) {
     throw new ValidationError("Product to update not found", "Product to be updated should exist");
   }
 
   //    arra of product variants
-  const oldProductVariant = record?.get("variant");
+  const oldProductVariant = JSON.parse(record?.get("variant"));
   const form = new RecordUpsertForm($app, record);
 
-  console.log(" = ========== oldproduct varaint  ===================== ===== ", oldProductVariant);
+  // console.log(" = ========== oldproduct varaint  ===================== ===== ", oldProductVariant);
   if (!oldProductVariant) {
     throw new ValidationError(
       "Product variant array to update not found",
@@ -64,6 +68,77 @@ onRecordBeforeCreateRequest((e) => {
   }
   const targetVariantIndex = oldProductVariant.findIndex((vrt) => vrt.size === productSize);
   // console.log("=========== target index  =============== ",targetVariantIndex)
+
+  if (targetVariantIndex === -1) {
+    //   update the product quantity
+    const newProductVariant = {
+      price: productPrice,
+      quantity: productQty,
+      size: productSize,
+    };
+
+    oldProductVariant.push(newProductVariant);
+    // or form.loadRequest(request, "")
+    form.loadData({
+      variant: oldProductVariant,
+    });
+    // validate and submit (internally it calls $app.dao().saveRecord(record) in a transaction)
+    form.submit();
+    return;
+  }
+  const newProductCount =
+			productQty + oldProductVariant[targetVariantIndex].quantity;
+  if(  newProductCount<0){
+    throw new ValidationError("Quantity cannot be less than zero", `Quantity cannot be less than zero ${newProductCount}`);
+  }
+  //   update the product quantity
+		const updatedProductVariant = {
+			...oldProductVariant[targetVariantIndex],
+			quantity: newProductCount,
+		};
+
+  oldProductVariant.splice(targetVariantIndex, 1, updatedProductVariant);
+  // or form.loadRequest(request, "")
+  form.loadData({
+    variant: oldProductVariant,
+  });
+  // validate and submit (internally it calls $app.dao().saveRecord(record) in a transaction)
+  form.submit();
+}, "liquorstore_products_inventory");
+
+//  ==============================================================
+// ======================= update hook ==========================
+//  ==============================================================
+
+onRecordBeforeUpdateRequest((e) => {
+  const productID = e.record?.get("product");
+  const productQty = e.record?.get("quantity");
+  const productSize = e.record?.get("size");
+  const productPrice = e.record?.get("price");
+
+  // console.log(" ==== productId ==== ", productID);
+  // console.log(" ==== quantity ==== ", productQty);
+  // console.log(" ==== size==== ", productSize);
+  // console.log(" ==== price==== ", productPrice);
+
+  if (!productID) {
+    throw new ValidationError(
+      "Product ID required",
+      "Product to be updated requires a valide product ID"
+    );
+  }
+  //   fupdate the record accordingly
+  const record = $app.dao().findRecordById("liquorstore_products", productID);
+  if (!record) {
+    throw new ValidationError("Product to update not found", "Product to be updated should exist");
+  }
+  //    array of product variants
+  const oldProductVariant = JSON.parse(record?.get("variant"));
+  const form = new RecordUpsertForm($app, record);
+
+  // console.log("=============== oldProductVariant ========================= ", oldProductVariant);
+  const targetVariantIndex = oldProductVariant.findIndex((vrt) => vrt.size === productSize);
+  // console.log("=========== target index  =============== ", targetVariantIndex);
 
   if (targetVariantIndex < 0) {
     //   update the product quantity
@@ -83,6 +158,14 @@ onRecordBeforeCreateRequest((e) => {
     return;
   }
 
+  const newProductCount =
+			productQty + oldProductVariant[targetVariantIndex].quantity;
+		if (newProductCount < 0) {
+			throw new ValidationError(
+				"Quantity cannot be less than zero",
+				`Quantity cannot be less than zero ${newProductCount}`,
+			);
+		}
   //   update the product quantity
   const updatedProductVariant = {
     ...oldProductVariant[targetVariantIndex],
@@ -98,73 +181,72 @@ onRecordBeforeCreateRequest((e) => {
   form.submit();
 }, "liquorstore_products_inventory");
 
-onRecordBeforeUpdateRequest((e) => {
-  const productID = e.record?.get("product");
-  const productQty = e.record?.get("quantity");
-  const productSize = e.record?.get("size");
-  if (!productID) {
-    throw new ValidationError(
-      "Product ID required",
-      "Product to be updated requires a valide product ID"
-    );
-  }
-  //   fupdate the record accordingly
-  const record = $app.dao().findRecordById("liquorstore_products", productID);
-  if (!record) {
-    throw new ValidationError("Product to update not found", "Product to be updated should exist");
-  }
-  //    arra of product variants
-  const oldProductVariant = JSON.parse(e.record?.get("variant"));
-  const form = new RecordUpsertForm($app, record);
-
-  const targetVariantIndex = oldProductVariant.findIndex((vrt) => vrt.size === productSize);
-
-  if (!targetVariantIndex || targetVariantIndex < 0) {
-    throw new ValidationError("Product to update not found", "Product to be updated should exist");
-  }
-
-  //   update the product quantity
-  const updatedProductVariant = {
-    ...oldProductVariant[targetVariantIndex],
-    quantity: productQty + oldProductVariant[targetVariantIndex].quantity,
-  };
-
-  oldProductVariant.splice(targetVariantIndex, 1, updatedProductVariant);
-  // or form.loadRequest(request, "")
-  form.loadData({
-    variant: JSON.stringify(oldProductVariant),
-  });
-  // validate and submit (internally it calls $app.dao().saveRecord(record) in a transaction)
-  form.submit();
-}, "liquorstore_products_inventory");
+//  ==============================================================
+// ======================= delete hook ==========================
+//  ==============================================================
 
 onRecordBeforeDeleteRequest((e) => {
   // const variant = JSON.parse(e.record?.get("variant"));
   const productID = e.record?.get("product");
   const productQty = e.record?.get("quantity");
   const productSize = e.record?.get("size");
+  const productPrice = e.record?.get("price");
+
   if (!productID) {
     throw new ValidationError(
       "Product ID required",
-      "Product to be updated requires a valide product ID"
+      "Product to be updated requires a valid product ID"
     );
   }
   //   fupdate the record accordingly
   const record = $app.dao().findRecordById("liquorstore_products", productID);
+  // console.log(" ==== record ==== ", record);
   if (!record) {
     throw new ValidationError("Product to update not found", "Product to be updated should exist");
   }
+
   //    arra of product variants
-  const oldProductVariant = JSON.parse(e.record?.get("variant"));
+  const oldProductVariant = JSON.parse(record?.get("variant"));
   const form = new RecordUpsertForm($app, record);
 
+  // console.log(" = ========== oldproduct varaint  ===================== ===== ", oldProductVariant);
+  if (!oldProductVariant) {
+    throw new ValidationError(
+      "Product variant array to update not found",
+      "Product variant array to be updated should exist"
+    );
+  }
   const targetVariantIndex = oldProductVariant.findIndex((vrt) => vrt.size === productSize);
 
-  if (!targetVariantIndex || targetVariantIndex < 0) {
-    throw new ValidationError("Product to update not found", "Product to be updated should exist");
-  }
+  // if (targetVariantIndex < 0) {
+		// 	// console.log("=========== target index  =============== ",targetVariantIndex)
+		//   //   update the product quantity
+		// 	const newProductVariant = {
+		// 		price: productPrice,
+		// 		quantity: productQty,
+		// 		size: productSize,
+		// 	};
 
-  //   update the product quantity
+		// 	oldProductVariant.push(newProductVariant);
+		// 	// or form.loadRequest(request, "")
+		// 	form.loadData({
+		// 		variant: oldProductVariant,
+		// 	});
+		// 	// validate and submit (internally it calls $app.dao().saveRecord(record) in a transaction)
+		// 	form.submit();
+		// 	return;
+		// }
+
+		//   update the product quantity
+
+		const newProductCount =
+			productQty + oldProductVariant[targetVariantIndex].quantity;
+		if (newProductCount < 0) {
+			throw new ValidationError(
+				"Quantity cannot be less than zero",
+				`Quantity cannot be less than zero ${newProductCount}`,
+			);
+		}
   const updatedProductVariant = {
     ...oldProductVariant[targetVariantIndex],
     quantity: productQty - oldProductVariant[targetVariantIndex].quantity,
@@ -172,7 +254,7 @@ onRecordBeforeDeleteRequest((e) => {
 
   oldProductVariant.splice(targetVariantIndex, 1, updatedProductVariant);
   // or form.loadRequest(request, "")
-  form.loadData({});
-  // validate and submit (internally it calls $app.dao().saveRecord(record) in a transaction)
-  form.submit();
+  form.loadData({
+    variant: oldProductVariant,
+  });
 }, "liquorstore_products_inventory");
